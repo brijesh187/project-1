b256d2b929f7f4bcb7233498f335074eb5f46414
brijesh187 <84964885+brijesh187@users.noreply.github.com>
2022-06-02 07:58:43 +0800

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,22 @@
+var firstNum = document.getElementById("first");
+var secondNum = document.getElementById("second");
+var Result = document.getElementById("final");
+
+
+const addd = document.getElementById("add");
+var sub = document.getElementById("sub");
+var mul = document.getElementById("mul");
+var div = document.getElementById("divide");
+
+
+
+addd.addEventListener("click", function(){
+
+ return {
+     firstNum + secondNum;
+ }
+
+
+    });
+
+
